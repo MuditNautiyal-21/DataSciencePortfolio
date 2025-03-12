1b9b73a9189da9c9ff6d5f2d93325ef264d0dee2
Mudit Nautiyal <muditnautiyalusa@gmail.com>
2025-03-12 15:36:47 -0400

    Updated python_basics.py

---
+++ python_basics.py
@@ -0,0 +1,20 @@
+# Python Basics for Data Science: I'm starting this module/repository to re-inforce my learnings in data science, to build a good portfolio.
+
+# 1. Variables and Data Types
+name = "Mudit" # String
+age = 30 # Integer (Can adjust age)
+height = 5.10 # Float (Adjustable)
+is_student = True
+
+print(f"Name: {name}, Age: {age}, Height: {height}, Student: {is_student}")
+
+
+# 2. Data Structures
+# lists - Ordered, mutable collection
+skills = ["Python", "SQL", "Data Analysis", "Machine Learning"]
+print(f"Skills: {skills}")
+print(f"First Skill: {skills[0]}")
+skills.append("Data Visualization")
+print(f"Updated Skills: {skills}")
+
+
